c13db89910e0e032c4b84993adbf415762d8d51a
NajibXY <naj.elkhadir@gmail.com>
2024-04-20 23:43:50 +0200

    training with random initial positions

--- unity/MLAgentsTests/Assets/Scripts/MoveToGoalBasic.cs
+++ unity/MLAgentsTests/Assets/Scripts/MoveToGoalBasic.cs
@@ -14,14 +14,14 @@ public class NewBehaviourScript : Agent {
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
 
-        Vector3 oldLocalPosition = transform.localPosition;
+        //Vector3 oldLocalPosition = transform.localPosition;
         transform.localPosition += 10f * Time.deltaTime * new Vector3(moveX, 0, moveZ);
         
-        if ((targetTransform.localPosition - transform.localPosition).sqrMagnitude < (targetTransform.localPosition - oldLocalPosition).sqrMagnitude) {
-            AddReward(0.1f);
-        } else {
-            AddReward(-0.1f);
-        }
+        //if ((targetTransform.localPosition - transform.localPosition).sqrMagnitude < (targetTransform.localPosition - oldLocalPosition).sqrMagnitude) {
+        //    AddReward(0.1f);
+        //} else {
+        //    AddReward(-0.1f);
+        //}
     }
 
     public override void CollectObservations(VectorSensor sensor) {
@@ -30,7 +30,8 @@ public class NewBehaviourScript : Agent {
     }
 
     public override void OnEpisodeBegin() {
-        transform.localPosition = Vector3.zero;
+        transform.localPosition = new Vector3(Random.Range(-2.5f, 0f), 0, Random.Range(-2f, 2f));
+        targetTransform.localPosition = new Vector3(Random.Range(1.5f, 5f), 0, Random.Range(-2f, 2f));
     }
 
     public override void Heuristic(in ActionBuffers actionsOut) {
